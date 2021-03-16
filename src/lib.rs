30dbbdd4abdbeacb1789d20f221ba1a4d564b27f
Nick <nickagerace@gmail.com>
2021-03-15 22:03:44 -0400

    Use unwrap_or for help and &str for run

--- src/lib.rs
+++ src/lib.rs
@@ -6,10 +6,7 @@ use std::io;
 
 /// This function prints general usage information to STDOUT for the `reelpath` CLI.
 pub fn help() {
-    let version = match option_env!("CARGO_PKG_VERSION") {
-        Some(s) => s,
-        None => "v?",
-    };
+    let version = option_env!("CARGO_PKG_VERSION").unwrap_or("v?");
     println!(
         "reelpath {}
 https://github.com/nickgerace/reelpath
@@ -24,8 +21,8 @@ USAGE:
 
 /// This function is the primary driver for the `reelpath` CLI. In errorless scenarios, this
 /// function will print its results to STDOUT. Otherwise, it will return any error encountered.
-pub fn run(s: &String) -> io::Result<()> {
-    match s.as_str() {
+pub fn run(s: &str) -> io::Result<()> {
+    match s {
         "-h" | "-help" | "--help" => help(),
         _ => match fs::canonicalize(s) {
             Ok(o) => println!("{}", o.display()),
