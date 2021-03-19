68cd4b779f62662d9ced67f83376f8d1ee474d88
Nick <nickagerace@gmail.com>
2021-03-18 22:17:52 -0400

    Add multi-arg support and move help to closure

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,6 @@ use std::io;
 
 fn main() -> io::Result<()> {
     let args: Vec<String> = env::args().collect();
-    match args.get(1) {
-        Some(s) => reelpath::run(s)?,
-        None => reelpath::help(),
-    }
+    reelpath::run(&args[1..args.len()])?;
     Ok(())
 }
